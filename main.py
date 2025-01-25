e34b64515982aafedb8ea5c9b53b13d161ab5e76
Shoxrux1308 <shohruxb1308@gmail.com>
2025-01-25 17:10:45 +0500

    count

--- main.py
+++ main.py
@@ -22,14 +22,27 @@ def start(update, context):
     update.message.reply_photo(photo=photo, reply_markup=reply_markup)
     
 
+def like(update, context):
+    
+    if update.callback_query.data == "like":
+        votes["likes"] += 1
+    elif update.callback_query.data == "dislike":
+        votes["dislikes"] += 1
 
+    keyboard = [
+        [InlineKeyboardButton(f"👍 Like {votes['likes']}", callback_data="like")],
+        [InlineKeyboardButton(f"👎 Dislike {votes['dislikes']}", callback_data="dislike")]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    update.callback_query.edit_message_reply_markup(reply_markup=reply_markup) 
 
 updater = Updater(TOKEN)  
 dispatcher = updater.dispatcher
 
 
 dispatcher.add_handler(CommandHandler("start", start))
-
+dispatcher.add_handler(CallbackQueryHandler(like))
 
 updater.start_polling()
 updater.idle()
